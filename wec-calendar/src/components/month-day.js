9bc07548875c4e94be71fc2badfba022bac29f9e
Dylan-Gordon <dylan.gordon.10@gmail.com>
2019-01-04 10:55:01 -0700

    Added code to monthDay

--- wec-calendar/src/components/month-day.js
+++ wec-calendar/src/components/month-day.js
@@ -1,7 +1,13 @@
 import React, {component } from 'react';
 
 class MonthDay extends Component {
-
+    render() {
+        return(
+            <div>
+            {this.props.day}
+            </div>
+        )
+    }
 }
 
 export default MonthDay;
