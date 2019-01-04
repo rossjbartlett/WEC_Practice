b7b6a31ccac456d5062feb51d8342c074cf4e1a1
Dylan-Gordon <dylan.gordon.10@gmail.com>
2019-01-04 11:13:52 -0700

    added month-week.js

---
+++ wec-calendar/src/components/month-week.js
@@ -0,0 +1,22 @@
+import React, {component } from 'react';
+import './month-day.js';
+class MonthWeek extends Component {
+    render() {
+        return(
+            <div>
+            {this.props.beginningDay}
+            {this.props.firstNumber}
+            </div>
+        )
+    }
+
+    generateDays(startDate){
+        let days = [];
+        for(let i=startDate; i < startDate+7; i++)
+        {
+            days.push(<monthDay day = {i}/>);
+        }
+    }
+}
+
+export default MonthWeek;
